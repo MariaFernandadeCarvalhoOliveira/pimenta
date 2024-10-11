d3087141a48b8e0793ecd349ee333fe178c6c75f
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-11 12:16:10 -0300

    yjh

--- perguntas.js
+++ perguntas.js
@@ -1,94 +1,94 @@
-criaCartao.(
-'Matemática,
+criaCartao(
+'Matemática' ,
     'Quantos graus tem um triangulo equilátero',
-    ''180)
+    '180')
     
-    criaCartao.(
-    'Matemática,
+    criaCartao(
+    'Matemática',
     'Quanto é 20-6?',
-    ''14)
+    '14')
     
-    criaCartao.(
-    'Matemática,
+    criaCartao(
+    'Matemática',
     'Qual é raiz quadrada de 625?',
-    ''25)
+    '25')
     
-    criaCartao.(
-    'Matemática,
-    'Quanto é 8×8?',
-    ''64)
+    criaCartao(
+    'Matemática',
+    'Quanto é 8 x 8?',
+    '64')
    
-    criaCartao.(
-    'Inglês,
-    Como se diz obrigado em inglês?,
-    Thanks)
+    criaCartao(
+    'Inglês',
+   'Como se diz obrigado em inglês?',
+    'Thanks')
     
-    criaCartao.(
-    'Inglês,
+    criaCartao(
+    'Inglês',
     'Como se diz verde em inglês?',
-    ''Green)
+    'Green')
     
-    criaCartao.(
-    'Inglês,
+    criaCartao(
+    'Inglês',
     'Como se diz gato em inglês?',
-    ''Cat)
+    'Cat')
     
-    criaCartao.(
-    'Inglês,
+    criaCartao(
+    'Inglês',
     'Como se diz carro em inglês?',
-    ''Car)
+    'Car')
     
-    criaCartao.(
-    'Inglês,
+    criaCartao(
+    'Inglês',
     'Como se diz caderno em inglês?',
-    ''Notbook)
+    'Notbook')
     
-    criaCartao.(
-    'Historia,
+    criaCartao(
+    'Historia',
     'Quem foi o primeiro imperador do Brasil?',
-    ''Dom Pedro I)
+    'Dom Pedro I')
     
-    criaCartao.(
-    'Historia,
+    criaCartao(
+    'Historia',
     'Qual evento deu início à Primeira Guerra Mundial?',
-    ''O assassinato do arquiduque Francisco Ferdinando, em 1914)
+    'O assassinato do arquiduque Francisco Ferdinando, em 1914')
     
-    criaCartao.(
-    'Historia,
+    criaCartao(
+    'Historia',
     'Em que ano terminou a Segunda Guerra Mundial?',
-    ''Em 1945.)
+    'Em 1945')
     
-    criaCartao.(
-    'Historia,
+    criaCartao(
+    'Historia',
     'Quando ocorreu a Revolução Francesa?',
-    ''1789)
+    '1789')
     
-    criaCartao.(
-    'Historia,
+    criaCartao(
+    'Historia',
     'O que foi o Tratado de Versalhes?',
-    ''Foi o tratado que encerrou oficialmente a Primeira Guerra Mundial em 1919.)
+    'Foi o tratado que encerrou oficialmente a Primeira Guerra Mundial em 1919')
     
-    criaCartao.(
-    'Geografia,
+    criaCartao(
+    'Geografia',
     'Qual é o rio mais longo do mundo?',
-    ''Rio Nilo)
+    'Rio Nilo')
     
-    criaCartao.(
-    'Geografia,
+    criaCartao(
+    'Geografia',
     'Qual é o maior oceano do planeta?',
-    ''Oceano Pacífico)
+    'Oceano Pacífico')
     
-    criaCartao.(
-    'Geografia,
+    criaCartao(
+    'Geografia',
     'Qual é o país conhecido como a "terra do sol nascente?',
-    ''Japão)
+    'Japão')
     
-    criaCartao.(
-    'Geografia,
+    criaCartao(
+    'Geografia',
     'Qual é o maior continente do mundo?',
-    ''Ásia)
+    'Ásia')
     
-    criaCartao.(
-    'Geografia,
+    criaCartao(
+    'Geografia',
     'Qual é o deserto mais seco do mundo?',
-    ''Deserto do Atacama))
+    'Deserto do Atacama')
