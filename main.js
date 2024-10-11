7d732b5dde3035f734d20239cb77daa00f58f481
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-11 12:06:16 -0300

    45

---
+++ main.js
@@ -0,0 +1,16 @@
+function criaCartao (categoria,pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.getElementById('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+    <div class="conteudo-cartao"
+<h3>categoria</h3>
+<div class="pergunta-cartao"></div>
+<p>perguntas</p>
+<div>
+    <div class="resposta-cartao">
+        <p>resposta</p>
+    </div>
+</div>
+    `
+}
