07c09964e8b03a1f92f23fb9fd6ff0b15fcd06d4
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-11 11:49:02 -0300

    xc

---
+++ perguntas.js
@@ -0,0 +1,94 @@
+criaCartao.(
+'Matemática,
+    'Quantos graus tem um triangulo equilátero',
+    ''180)
+    
+    criaCartao.(
+    'Matemática,
+    'Quanto é 20-6?',
+    ''14)
+    
+    criaCartao.(
+    'Matemática,
+    'Qual é raiz quadrada de 625?',
+    ''25)
+    
+    criaCartao.(
+    'Matemática,
+    'Quanto é 8×8?',
+    ''64)
+   
+    criaCartao.(
+    'Inglês,
+    'Como se diz obrigado em inglês?',
+    ''Thanks)
+    
+    criaCartao.(
+    'Inglês,
+    'Como se diz verde em inglês?',
+    ''Green)
+    
+    criaCartao.(
+    'Inglês,
+    'Como se diz gato em inglês?',
+    ''Cat)
+    
+    criaCartao.(
+    'Inglês,
+    'Como se diz carro em inglês?',
+    ''Car)
+    
+    criaCartao.(
+    'Inglês,
+    'Como se diz caderno em inglês?',
+    ''Notbook)
+    
+    criaCartao.(
+    'Historia,
+    'Quem foi o primeiro imperador do Brasil?',
+    ''Dom Pedro I)
+    
+    criaCartao.(
+    'Historia,
+    'Qual evento deu início à Primeira Guerra Mundial?',
+    ''O assassinato do arquiduque Francisco Ferdinando, em 1914)
+    
+    criaCartao.(
+    'Historia,
+    'Em que ano terminou a Segunda Guerra Mundial?',
+    ''Em 1945.)
+    
+    criaCartao.(
+    'Historia,
+    'Quando ocorreu a Revolução Francesa?',
+    ''1789)
+    
+    criaCartao.(
+    'Historia,
+    'O que foi o Tratado de Versalhes?',
+    ''Foi o tratado que encerrou oficialmente a Primeira Guerra Mundial em 1919.)
+    
+    criaCartao.(
+    'Geografia,
+    'Qual é o rio mais longo do mundo?',
+    ''Rio Nilo)
+    
+    criaCartao.(
+    'Geografia,
+    'Qual é o maior oceano do planeta?',
+    ''Oceano Pacífico)
+    
+    criaCartao.(
+    'Geografia,
+    'Qual é o país conhecido como a "terra do sol nascente?',
+    ''Japão)
+    
+    criaCartao.(
+    'Geografia,
+    'Qual é o maior continente do mundo?',
+    ''Ásia)
+    
+    criaCartao.(
+    'Geografia,
+    'Qual é o deserto mais seco do mundo?',
+    ''Deserto do Atacama))
