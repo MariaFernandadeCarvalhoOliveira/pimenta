cf56e22e7ba7f6e9f60b1a1d59b2f9588f388610
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-11-11 08:37:57 -0300

    vh

--- projeto2.js
+++ projeto2.js
@@ -15,7 +15,7 @@ function criaCartao (categoria,pergunta, resposta){
 container.appendChild(cartao);
 }
 
-function mostrarResposta (card){
+function MostrarResposta (card){
     const resposta = card.querySelector('.resposta-cartao')
     if(resposta.style.display === 'none'){
         resposta.style.display = 'block'
