f3fc3d4bbc941ffa75fb240b7320b9c52fecbdb5
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-11-11 08:21:11 -0300

    ui

--- projeto2.js
+++ projeto2.js
@@ -2,7 +2,7 @@ function criaCartao (categoria,pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article')
     cartao.className = 'cartao';
-    cartao.innerHTML = `<div class="conteudo-cartao">
+    cartao.innerHTML = `<div class="conteudo-cartao"onclik="MostrarResposta(this)">
 <h3>${categoria}</h3>
 <div class="pergunta-cartao">
     <p>${pergunta}</p>
@@ -13,4 +13,14 @@ function criaCartao (categoria,pergunta, resposta){
 </div>
 `
 container.appendChild(cartao);
+}
+
+function mostrarResposta (card){
+    const resposta = card.querySelector('.resposta-cartao')
+    if(resposta.style.display === 'none'){
+        resposta.style.display = 'block'
+    }
+    else{ resposta.style.display = 'none'
+    }
+
 }
